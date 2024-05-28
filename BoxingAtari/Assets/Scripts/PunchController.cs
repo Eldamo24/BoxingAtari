fc17fab2ce2f4e3aaf9bcbf07900a251b61cda5d
Damian Lobos <damianlobos24@gmail.com>
2024-05-28 10:36:26 -0300

    Empuje del enemigo al golpearlo

--- BoxingAtari/Assets/Scripts/PunchController.cs
+++ BoxingAtari/Assets/Scripts/PunchController.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class PunchController : MonoBehaviour
 {
+    public float pushDuration = 1f; 
+    public float forceMagnitude = 30f; 
+    private Vector2 pushDirection;
+    private float pushTimeRemaining = 0f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponentInParent<Rigidbody2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Punch") && collision.gameObject.name == "SpritePlayer")
@@ -9,11 +20,26 @@ public class PunchController : MonoBehaviour
             collision.gameObject.GetComponentInParent<PlayerController>().Hit++;
             if(collision.gameObject.GetComponentInParent<PlayerController>().Hit == 1)
             {
+                pushDuration = 1f;
                 gameObject.GetComponentInParent<EnemyController>().Punched();
                 collision.gameObject.GetComponentInParent<PlayerController>().PlayerPoints++;
                 UIController.instance.UpdateScorePlayer();
+                pushDirection = (transform.parent.transform.position - collision.transform.position).normalized;
+                pushTimeRemaining = pushDuration;
             }
 
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (pushTimeRemaining > 0)
+        {
+
+            pushTimeRemaining -= Time.fixedDeltaTime;
+
+            Vector2 newPosition = rb.position + pushDirection * forceMagnitude * Time.fixedDeltaTime;
+            rb.MovePosition(newPosition);
         }
     }
 }
