f878a6cdb32eb601b4597b201737888756434637
Damian Lobos <damianlobos24@gmail.com>
2024-05-28 08:09:56 -0300

    Correcciones

--- BoxingAtari/Assets/Scripts/EnemyController.cs
+++ BoxingAtari/Assets/Scripts/EnemyController.cs
@@ -1,8 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
--- BoxingAtari/Assets/Scripts/UIController.cs
+++ BoxingAtari/Assets/Scripts/UIController.cs
@@ -1,9 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
